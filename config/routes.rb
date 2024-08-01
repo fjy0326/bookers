1f2b5768a354bbc403bbc50009811b896eed9a0d
fjy0326 <bungakukai2010@gmail.com>
2024-08-01 02:21:42 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get '/books' => 'books#new'
-  post 'books' => 'books#create' #追加記述（分からないから消すかも）
-  get 'books/create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get '/top' => 'homes#top'
+  root 'homes#top'
+  get '/' => 'homes#top'
+  resources :books
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
